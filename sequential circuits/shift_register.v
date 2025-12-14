5e3412c25fbac5057c3f16cab3b72d46875a613d
Srujan Prasad <srujanprasad2004@gmail.com>
2025-12-14 14:43:08 +0530

    Update shift_register.v

--- sequential circuits/shift_register.v
+++ sequential circuits/shift_register.v
@@ -1,1 +1,18 @@
-
+```verilog
+module shiftreg_2bit(input clk,rst,d,output reg q,q0);
+  always @(posedge clk or negedge rst)
+    begin
+      if(~rst)
+        begin
+        q<=0;
+        q0<=0;
+        end
+      else
+        begin
+          q0<=q;
+          q0<=d;
+        end
+    end
+endmodule
+```
+      
