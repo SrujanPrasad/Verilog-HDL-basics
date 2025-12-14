ced3a6612927328fd1002ee2d36b5c507c47956d
Srujan Prasad <srujanprasad2004@gmail.com>
2025-12-14 14:48:14 +0530

    Create synthesis_error.v

---
+++ sequential circuits/synthesis_error.v
@@ -0,0 +1,14 @@
+module synerror(input clk,rst,d,output q0,q);
+  always @(posedge clk or negedge rst)
+    begin
+      if(~rst)
+        begin
+          q0=0;
+          q=0;
+        end
+      else
+        begin
+          q0=d;
+          q=q0;
+        end
+  endmodule 
